ef6444d3f4fc7a3863528a7aff5807451f9a084c
Rafael Cardeal Lodi <65565453+rafaelcardeallodi@users.noreply.github.com>
2022-04-01 08:58:51 -0300

    Utilizando StyleSheet

--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -1,16 +1,59 @@
 import React from 'react';
-import { Text, View } from 'react-native';
+import { Platform, StyleSheet, Text, TextInput, TouchableOpacity, View } from 'react-native';
 
 export function Home(){
   return(
-    <View  style={{
-      flex: 1,
-      justifyContent: 'center',
-      alignItems: 'center'
-    }}>
-      <Text>Hello World!</Text>
+    <View style={styles.container}>
+      <Text style={styles.title}>Welcome, Rafael</Text>
+      <TextInput 
+        style={styles.input}
+        placeholder="New skill"
+        placeholderTextColor="#555"
+      />
+      <TouchableOpacity 
+        style={styles.button}
+        activeOpacity={0.7}
+      >
+        <Text style={styles.buttonText}>Add</Text>
+      </TouchableOpacity>
 
-      <Text>Ignite</Text>
+      <Text style={[styles.title, { marginTop: 50 }]}>
+        My Skills
+      </Text>
     </View>
   )
 }
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    backgroundColor: '#121015',
+    paddingVertical: 70,
+    paddingHorizontal: 30
+  },
+  title: {
+    color: '#fff',
+    fontSize: 24,
+    fontWeight: 'bold'
+  },
+  input: {
+    backgroundColor: '#1F1e25',
+    color: '#fff',
+    fontSize: 18,
+    padding: Platform.OS === 'ios' ? 15 : 10,
+    marginTop: 30,
+    borderRadius: 7
+  },
+  button: {
+    backgroundColor: '#a370f7',
+    padding: 15,
+    borderRadius: 7,
+    marginTop: 20,
+    alignItems: 'center'
+  },
+  buttonText: {
+    color: '#fff',
+    fontSize: 17,
+    fontWeight: 'bold'
+  }
+})
